b1cdb82ef8d3d5418f03ba59d3b653641b04c2e5
wzy <wangzhiyuan@bianxianmao.com>
2023-09-03 19:51:15 +0800

    feat：修改生产者和消费者案例代码

--- rocketmq/src/main/java/com/wzy/PushConsumerExample.java
+++ rocketmq/src/main/java/com/wzy/PushConsumerExample.java
@@ -11,6 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 
 public class PushConsumerExample {
@@ -21,19 +24,19 @@ public class PushConsumerExample {
 
     public static void main(String[] args) throws ClientException, IOException, InterruptedException {
         final ClientServiceProvider provider = ClientServiceProvider.loadService();
-        // 接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8081;xxx:8081。
+        // 1、接入点地址，需要设置成Proxy的地址和端口列表，一般是xxx:8081;xxx:8081。
         String endpoints = "localhost:8081";
         ClientConfiguration clientConfiguration = ClientConfiguration.newBuilder()
                 .setEndpoints(endpoints)
                 .build();
-        // 订阅消息的过滤规则，表示订阅所有Tag的消息。
+        // 2、订阅消息的过滤规则，表示订阅所有Tag的消息。
         String tag = "*";
         FilterExpression filterExpression = new FilterExpression(tag, FilterExpressionType.TAG);
-        // 为消费者指定所属的消费者分组，Group需要提前创建。
-        String consumerGroup = "YourConsumerGroup";
-        // 指定需要订阅哪个目标Topic，Topic需要提前创建。
+        // 3、为消费者指定所属的消费者分组，Group需要提前创建。
+        String consumerGroup = "MyConsumerGroup";
+        // 4、指定需要订阅哪个目标Topic，Topic需要提前创建。
         String topic = "TestTopic";
-        // 初始化PushConsumer，需要绑定消费者分组ConsumerGroup、通信参数以及订阅关系。
+        // 5、初始化PushConsumer，需要绑定消费者分组ConsumerGroup、通信参数以及订阅关系。
         PushConsumer pushConsumer = provider.newPushConsumerBuilder()
                 .setClientConfiguration(clientConfiguration)
                 // 设置消费者分组。
@@ -43,12 +46,19 @@ public class PushConsumerExample {
                 // 设置消费监听器。
                 .setMessageListener(messageView -> {
                     // 处理消息并返回消费结果。
-                    logger.info("Consume message successfully, messageId={}", messageView.getMessageId());
+                    try {
+                        logger.info("Consume message successfully, messageId={}", messageView.getMessageId());
+                        Charset charset = StandardCharsets.UTF_8;
+                        CharBuffer charBuffer = charset.decode(messageView.getBody());
+                        logger.info("MessageBody is :{}", charBuffer);
+                    } catch (Exception ex) {
+                        logger.error("转换消息内容错误：", ex);
+                    }
                     return ConsumeResult.SUCCESS;
                 })
                 .build();
         Thread.sleep(Long.MAX_VALUE);
-        // 如果不需要再使用 PushConsumer，可关闭该实例。
-        // pushConsumer.close();
+        // 6、如果不需要再使用 PushConsumer，可关闭该实例。
+        pushConsumer.close();
     }
 }
