f64fa041893cb89a9753844677f2341582924d43
wzy <842089160@qq.com>
2024-01-21 20:47:36 +0800

    feat：添加guava例子

---
+++ jdk/src/main/java/com/wzy/guava/eventbus/OrderEventListener.java
@@ -0,0 +1,10 @@
+package com.wzy.guava.eventbus;
+
+import com.google.common.eventbus.Subscribe;
+
+public class OrderEventListener {
+    @Subscribe
+    public void dealWithEvent(OrderMessage event) {
+        System.out.println("收到订单事件，内容：" + event);
+    }
+}
---
+++ jdk/src/main/java/com/wzy/guava/eventbus/OrderMessage.java
@@ -0,0 +1,21 @@
+package com.wzy.guava.eventbus;
+
+public class OrderMessage {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderMessage{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
